cd583c30f58aed6496a23eff34fa5f944f098895
Juan Díaz <juan.diaz.12069@gmail.com>
2020-05-02 16:44:47 -0400

    playground, handlind arrays

--- part1/playground/complex-state/src/index.js
+++ part1/playground/complex-state/src/index.js
@@ -2,29 +2,27 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [click, setClick] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const handleLeftClick = () =>
-    setClick({
-      ...click,
-      left: click.left + 1,
-    });
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
 
-  const handleRightClick = () =>
-    setClick({
-      ...click,
-      right: click.right + 1,
-    });
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
 
   return (
     <>
-      {click.left}
+      {left}
       <button onClick={handleLeftClick}>Left</button>
-      {click.right}
+      {right}
       <button onClick={handleRightClick}>Right</button>
+      <p>{allClicks.join(' ')}</p>
     </>
   );
 };
