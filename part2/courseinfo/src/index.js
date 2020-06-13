a1eda05707f164cbc3c24b935f08ef2b8f98c88e
Juan Diaz <juan.diaz.12069@gmail.com>
2020-06-12 22:10:30 -0400

    part2: exercise 2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -50,33 +50,54 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        id: 2,
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        id: 3,
-        name: 'State of a component',
-        exercises: 14,
-      },
-      {
-        id: 4,
-        name: 'Redux',
-        exercises: 18,
-      },
-    ],
-  };
-  return <Course course={course} />;
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          id: 1,
+          name: 'Fundamentals of React',
+          exercises: 10,
+        },
+        {
+          id: 2,
+          name: 'Using props to pass data',
+          exercises: 7,
+        },
+        {
+          id: 3,
+          name: 'State of a component',
+          exercises: 14,
+        },
+        {
+          id: 4,
+          name: 'Redux',
+          exercises: 18,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return courses.map((course) => {
+    return <Course key={course.id} course={course} />;
+  });
 };
 
 ReactDOM.render(
