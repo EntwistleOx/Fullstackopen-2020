da389eaae46000549fb3a145a3cd78fcecf71625
Juan Díaz <juan.diaz.12069@gmail.com>
2020-06-30 23:58:01 -0400

    part3: exercise 3.11, full stack!

---
+++ part3/phonebook/phonebook-backend/build/precache-manifest.1783e876512332f61e64a80efb4aa158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8b912aee123593f02b7c82d0162bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed59969fbb9696ccf908",
+    "url": "/static/css/main.6874a02b.chunk.css"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed59969fbb9696ccf908",
+    "url": "/static/js/main.7006b424.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -39,6 +39,7 @@ morgan.token('body', (req, res) => {
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
+app.use(express.static('build'));
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
