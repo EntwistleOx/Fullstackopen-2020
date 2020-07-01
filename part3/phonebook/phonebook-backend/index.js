6ce3221619d390c4d8c8cbfab72afc19eb13dfdc
Juan Díaz <juan.diaz.12069@gmail.com>
2020-06-30 23:39:32 -0400

    part3: exercise 3.9

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -37,6 +38,7 @@ morgan.token('body', (req, res) => {
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -80,7 +82,7 @@ app.post('/api/persons', (req, res) => {
   };
 
   persons = persons.concat(person);
-  res.json(persons);
+  res.json(person);
 });
 
 app.get('/api/persons', (req, res) => {
@@ -106,7 +108,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
