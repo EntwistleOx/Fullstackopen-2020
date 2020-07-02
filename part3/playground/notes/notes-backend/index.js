13e48347b39b54ffea0cfadd294700b7dbf3fe89
Juan Díaz <juan.diaz.12069@gmail.com>
2020-07-01 22:34:26 -0400

    connect mongo

--- part3/playground/notes/notes-backend/index.js
+++ part3/playground/notes/notes-backend/index.js
@@ -1,37 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
+const Note = require('./models/note');
 const app = express();
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-];
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
 
-const genId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+// const genId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.get('/', (req, res) => {
   res.send('Hello humans! 👽');
@@ -46,29 +27,38 @@ app.post('/api/notes', (req, res) => {
     });
   }
 
-  const note = {
-    id: genId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-  };
-  notes = notes.concat(note);
-  res.json(note);
+  });
+
+  // notes = notes.concat(note);
+  // res.json(note);
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  // res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
+  const id = req.params.id;
+  // const note = notes.find((note) => note.id === id);
 
-  if (note) {
+  // if (note) {
+  //   res.json(note);
+  // } else {
+  //   res.status(404).end();
+  // }
+  Note.findById(id).then((note) => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -77,7 +67,7 @@ app.delete('/api/notes/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
