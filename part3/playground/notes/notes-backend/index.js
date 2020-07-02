6df298fb8f5fd901d0f3d20b343e1287d3de93b5
Juan Díaz <juan.diaz.12069@gmail.com>
2020-07-02 01:22:25 -0400

    error handling

--- part3/playground/notes/notes-backend/index.js
+++ part3/playground/notes/notes-backend/index.js
@@ -5,14 +5,9 @@ require('dotenv').config();
 const Note = require('./models/note');
 const app = express();
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'));
-
-// const genId = () => {
-//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-//   return maxId + 1;
-// };
 
 app.get('/', (req, res) => {
   res.send('Hello humans! 👽');
@@ -33,39 +28,77 @@ app.post('/api/notes', (req, res) => {
     date: new Date(),
   });
 
-  // notes = notes.concat(note);
-  // res.json(note);
   note.save().then((savedNote) => {
     res.json(savedNote);
   });
 });
 
 app.get('/api/notes', (req, res) => {
-  // res.json(notes);
   Note.find({}).then((notes) => {
     res.json(notes);
   });
 });
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
   const id = req.params.id;
-  // const note = notes.find((note) => note.id === id);
 
-  // if (note) {
-  //   res.json(note);
-  // } else {
-  //   res.status(404).end();
-  // }
-  Note.findById(id).then((note) => {
-    res.json(note);
-  });
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body;
+  const id = req.params.id;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  // notes = notes.filter((note) => note.id !== id);
+  // res.status(204).end();
+  Note.findByIdAndRemove(id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
